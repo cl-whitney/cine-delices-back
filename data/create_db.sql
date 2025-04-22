5d191949d3745b0cb2296202d3de11dc6cd93542
Nicolas-Dabusse <nico43d@hotmail.fr>
2025-04-22 13:15:15 +0200

    docker: create dockerfile and docker compose OK

--- data/create_db.sql
+++ data/create_db.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS "category", "quantity", "ingredient", "media", "user";
 DROP DOMAIN IF EXISTS "email";
 
 -- Regex pour le contrôle des emails utilisateurs 
-CREATE DOMAIN IF EXISTS "email" AS text
+CREATE DOMAIN "email" AS text
 CHECK(
     value ~ '(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
 );
