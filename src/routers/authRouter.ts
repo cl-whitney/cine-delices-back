2ed49659d67c55e50085d8123edb93f6e2227963
Nicolas-Dabusse <nico43d@hotmail.fr>
2025-04-27 10:15:38 +0200

    fix Front logout bug

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import loginController from '../controllers/loginController';
 import registerController from '../controllers/registerController';
-import isAuth from '../middlewares/isAuth';
 
 const authRouter = Router();
 
 authRouter.post('/inscription', registerController.signup);
 authRouter.post('/connexion', loginController.login)
-authRouter.delete('/deconnexion', isAuth, loginController.logout)
+authRouter.delete('/deconnexion', loginController.logout)
 
 export default authRouter
